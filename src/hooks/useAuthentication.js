50f522c896b3384e7f98d2023cdf74fe0dc58237
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-12 04:23:13 -0300

    adiciona função de logout e cria forma de login

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -59,6 +59,12 @@ export const useAuthentication = () => {
 
   };
 
+  //logout
+  const logout = () => {
+    checkIfIsCancelled();
+    signOut(auth);
+  }
+
   useEffect(() => {
     return () => setCancceled(true);
   }, []);
@@ -68,5 +74,6 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
+    logout
   }
 };
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./About.module.css"
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>Sobre a <span>EnsinomiBlog</span>
+      </h2>
+      <p>Este projeto consiste em uma aplicação feita com React no front-end e Firebase no back-end</p>
+      <Link to="/posts/create" className="btn">Criar post</Link>
     </div>
   )
 }
